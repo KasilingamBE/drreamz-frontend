4ce9c269e5fb4d6bf13697467fa844f39787fe80
Bilal <bilalmomin39@gmail.com>
2021-02-01 22:51:01 +0530

    homepage

--- packages/web/src/app/components/home/HomePage.jsx
+++ packages/web/src/app/components/home/HomePage.jsx
@@ -5,20 +5,18 @@ import { showLoading, hideLoading } from "react-redux-loading";
 import { useMutation, gql } from "@apollo/client";
 import { client } from "../../graphql/index";
 
-const RUN_EMAIL_SUBMIT_TASK = gql`
-  mutation RunEmailSubmitTask(
-
-    $Email_id: String!
-
+const RUN_HOMEPAGE_EMAIL_TASK = gql`
+  mutation RunHomepageEmailTask(
+    $Email_id: String!  
   ) {
-    runEmailSubmitTask(
+    runHomepageEmailTask(
       Email_id: $Email_id
     )
   }
 `;
 
 const HomePage = ({ dispatch, authenticated, userId }) => {
-    const [runEmailSubmitTask] = useMutation(RUN_EMAIL_SUBMIT_TASK);
+    const [runHomepageEmailTask] = useMutation(RUN_HOMEPAGE_EMAIL_TASK);
     const [disabled, setDisabled] = useState(false);
     const [payload, setPayload] = useState({
       Email_id: "",
@@ -29,7 +27,7 @@ const HomePage = ({ dispatch, authenticated, userId }) => {
           e.preventDefault();
           dispatch(showLoading());
           setDisabled(true);
-          const response = await runLinkedinRequestTask({
+          const response = await runHomepageEmailTask({
             variables: {
               Email_id: payload.Email_id,
             },
@@ -37,7 +35,7 @@ const HomePage = ({ dispatch, authenticated, userId }) => {
           
           dispatch(hideLoading());
           setDisabled(false);
-          if (response.data.runLinkedinRequestTask.includes("Submitting Email")) {
+          if (response.data.runHomepageEmailTask.includes("Submitting Email")) {
             alert("Submitting Email");
           } else {
             alert("Something went wrong please try again");
@@ -45,62 +43,63 @@ const HomePage = ({ dispatch, authenticated, userId }) => {
         } catch (error) {
           dispatch(hideLoading());
           setDisabled(false);
+          // console.log(response)
           console.log(userId)
           console.log(error)
-          alert("Something went wrong please try again");
+          alert("Something went wrong please try againnnnnn");
         }
       };
 
-      return (
-        <div className="py-5">
-          <h1 className="text-center">Home Page</h1>
-    
-          <Form className="mt-3 container" onSubmit={handleSubmit}>
-            
-            <FormGroup>
-              <Input
-                type="text"
-                name="Email_id"
-                placeholder="Email_id"
-                value={payload.Email_id}
-                onChange={(e) =>
-                  setPayload({ ...payload, Email_id: e.target.value })
-                }
-                required
-              />
-            </FormGroup>
-            <Button
-              type="submit"
-              style={{ pointerEvents: disabled ? "none" : "auto" }}
-              color="primary"
-              block
-            >
-              {disabled ? (
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
-                />
-              ) : (
-                "Submit"
-              )}
-            </Button>
-          </Form>
-          
-                  
-        </div>
-      );
-    };
-    
-    const mapStateToProps = ({ auth }) => {
-      return {
-        authenticated: auth.authenticated,
-        userId: auth.authenticated ? auth.data.attributes.sub : null,
+  return (
+    <div className="py-5">
+      <h1 className="text-center">Home Page</h1>
+
+      <Form className="mt-3 container" onSubmit={handleSubmit}>
         
-      };
-    };
+        <FormGroup>
+          <Input
+            type="text"
+            name="Email_id"
+            placeholder="Email_id"
+            value={payload.Email_id}
+            onChange={(e) =>
+              setPayload({ ...payload, Email_id: e.target.value })
+            }
+            required
+          />
+        </FormGroup>
+        <Button
+          type="submit"
+          style={{ pointerEvents: disabled ? "none" : "auto" }}
+          color="primary"
+          block
+        >
+          {disabled ? (
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+          ) : (
+            "Submit"
+          )}
+        </Button>
+      </Form>
+      
+              
+    </div>
+  );
+};
     
-    export default connect(mapStateToProps)(HomePage);
+const mapStateToProps = ({ auth }) => {
+  return {
+    authenticated: auth.authenticated,
+    userId: auth.authenticated ? auth.data.attributes.sub : null,
+    
+  };
+};
+
+export default connect(mapStateToProps)(HomePage);
     
