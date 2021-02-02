42ffd2d02d3073fd96eddb9477d0813d6587b0ce
Bilal <bilalmomin39@gmail.com>
2021-02-02 14:50:01 +0530

    homepage redirect added

---
+++ packages/web/pages/thank-you/index.js
@@ -0,0 +1,13 @@
+import UserLayout from '../../src/app/components/other/UserLayout';
+
+export default function Home() {
+  return (
+    <UserLayout pageTitle="Dreamjobpal | Thank you">
+      <div className="py-5">
+        <div>
+          <h1 className="text-center">THANK YOU</h1>
+        </div>
+      </div>
+    </UserLayout>
+  );
+}
--- packages/web/src/app/components/home/HomePage.jsx
+++ packages/web/src/app/components/home/HomePage.jsx
@@ -1,105 +1,193 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { Button, Form, FormGroup, Input, Spinner } from "reactstrap";
-import { showLoading, hideLoading } from "react-redux-loading";
-import { useMutation, gql } from "@apollo/client";
-import { client } from "../../graphql/index";
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Button, Form, FormGroup, Input, Spinner } from 'reactstrap';
+import { showLoading, hideLoading } from 'react-redux-loading';
+import { useMutation, gql } from '@apollo/client';
+import { useRouter } from 'next/router';
 
 const RUN_HOMEPAGE_EMAIL_TASK = gql`
-  mutation RunHomepageEmailTask(
-    $Email_id: String!  
-  ) {
-    runHomepageEmailTask(
-      Email_id: $Email_id
-    )
+  mutation RunHomepageEmailTask($Email_id: String!) {
+    runHomepageEmailTask(Email_id: $Email_id)
   }
 `;
 
 const HomePage = ({ dispatch, authenticated, userId }) => {
-    const [runHomepageEmailTask] = useMutation(RUN_HOMEPAGE_EMAIL_TASK);
-    const [disabled, setDisabled] = useState(false);
-    const [payload, setPayload] = useState({
-      Email_id: "",
-    });
+  const [runHomepageEmailTask] = useMutation(RUN_HOMEPAGE_EMAIL_TASK);
+  const [disabled, setDisabled] = useState(false);
+  const [payload, setPayload] = useState({
+    Email_id: ''
+  });
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-        try {
-          e.preventDefault();
-          dispatch(showLoading());
-          setDisabled(true);
-          const response = await runHomepageEmailTask({
-            variables: {
-              Email_id: payload.Email_id,
-            },
-          });
-          
-          dispatch(hideLoading());
-          setDisabled(false);
-          // if (response.data.runHomepageEmailTask.includes("Submitting Email")) {
-          alert("Submitting Email");
-          // } else {
-          //   alert("Something went wrong please try again");
-          // }
-        } catch (error) {
-          dispatch(hideLoading());
-          setDisabled(false);
-          // console.log(response)
-          console.log(userId)
-          console.log(error)
-          alert("Something went wrong please try againnnnnn");
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      dispatch(showLoading());
+      setDisabled(true);
+      await runHomepageEmailTask({
+        variables: {
+          Email_id: payload.Email_id
         }
-      };
+      });
+
+      dispatch(hideLoading());
+      setDisabled(false);
+      router.push('/thank-you');
+    } catch (error) {
+      dispatch(hideLoading());
+      setDisabled(false);
+      // console.log(response)
+      // console.log(userId);
+      // console.log(error);
+      alert('Something went wrong please try againnnnnn');
+    }
+  };
 
   return (
     <div className="py-5">
       <h1 className="text-center">Home Page</h1>
-
       <Form className="mt-3 container" onSubmit={handleSubmit}>
-        
         <FormGroup>
           <Input
             type="text"
             name="Email_id"
             placeholder="Email_id"
             value={payload.Email_id}
-            onChange={(e) =>
-              setPayload({ ...payload, Email_id: e.target.value })
-            }
+            onChange={(e) => setPayload({ ...payload, Email_id: e.target.value })}
             required
           />
         </FormGroup>
         <Button
           type="submit"
-          style={{ pointerEvents: disabled ? "none" : "auto" }}
+          style={{ pointerEvents: disabled ? 'none' : 'auto' }}
           color="primary"
-          block
-        >
+          block>
           {disabled ? (
-            <Spinner
-              as="span"
-              animation="border"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-            />
+            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
           ) : (
-            "Submit"
+            'Submit'
           )}
         </Button>
       </Form>
-      
-              
     </div>
   );
 };
-    
+
 const mapStateToProps = ({ auth }) => {
   return {
     authenticated: auth.authenticated,
-    userId: auth.authenticated ? auth.data.attributes.sub : null,
-    
+    userId: auth.authenticated ? auth.data.attributes.sub : null
   };
 };
 
 export default connect(mapStateToProps)(HomePage);
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { connect } from "react-redux";
+// import { Button, Form, FormGroup, Input, Spinner } from "reactstrap";
+// import { showLoading, hideLoading } from "react-redux-loading";
+// import { useMutation, gql } from "@apollo/client";
+// import { client } from "../../graphql/index";
+
+// const RUN_HOMEPAGE_EMAIL_TASK = gql`
+//   mutation RunHomepageEmailTask(
+//     $Email_id: String!  
+//   ) {
+//     runHomepageEmailTask(
+//       Email_id: $Email_id
+//     )
+//   }
+// `;
+
+// const HomePage = ({ dispatch, authenticated, userId }) => {
+//     const [runHomepageEmailTask] = useMutation(RUN_HOMEPAGE_EMAIL_TASK);
+//     const [disabled, setDisabled] = useState(false);
+//     const [payload, setPayload] = useState({
+//       Email_id: "",
+//     });
+
+//     const handleSubmit = async (e) => {
+//         try {
+//           e.preventDefault();
+//           dispatch(showLoading());
+//           setDisabled(true);
+//           const response = await runHomepageEmailTask({
+//             variables: {
+//               Email_id: payload.Email_id,
+//             },
+//           });
+          
+//           dispatch(hideLoading());
+//           setDisabled(false);
+//           // if (response.data.runHomepageEmailTask.includes("Submitting Email")) {
+//           alert("Submitting Email");
+//           // } else {
+//           //   alert("Something went wrong please try again");
+//           // }
+//         } catch (error) {
+//           dispatch(hideLoading());
+//           setDisabled(false);
+//           // console.log(response)
+//           console.log(userId)
+//           console.log(error)
+//           alert("Something went wrong please try againnnnnn");
+//         }
+//       };
+
+//   return (
+//     <div className="py-5">
+//       <h1 className="text-center">Home Page</h1>
+
+//       <Form className="mt-3 container" onSubmit={handleSubmit}>
+        
+//         <FormGroup>
+//           <Input
+//             type="text"
+//             name="Email_id"
+//             placeholder="Email_id"
+//             value={payload.Email_id}
+//             onChange={(e) =>
+//               setPayload({ ...payload, Email_id: e.target.value })
+//             }
+//             required
+//           />
+//         </FormGroup>
+//         <Button
+//           type="submit"
+//           style={{ pointerEvents: disabled ? "none" : "auto" }}
+//           color="primary"
+//           block
+//         >
+//           {disabled ? (
+//             <Spinner
+//               as="span"
+//               animation="border"
+//               size="sm"
+//               role="status"
+//               aria-hidden="true"
+//             />
+//           ) : (
+//             "Submit"
+//           )}
+//         </Button>
+//       </Form>
+      
+              
+//     </div>
+//   );
+// };
+    
+// const mapStateToProps = ({ auth }) => {
+//   return {
+//     authenticated: auth.authenticated,
+//     userId: auth.authenticated ? auth.data.attributes.sub : null,
+    
+//   };
+// };
+
+// export default connect(mapStateToProps)(HomePage);
     
