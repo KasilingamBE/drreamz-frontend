188b794d2e2984a05ea24ea5ebc4b0e3904243f4
vivekvt <vivek78643@gmail.com>
2021-02-02 13:37:56 +0530

    form submit redirect

--- packages/web/pages/homepage/index.js
+++ packages/web/pages/homepage/index.js
@@ -1,29 +1,17 @@
-// import Layout from "../../../components/Layout";
-// import PageHeader from "../../../components/PageHeader";
-// import Footer from "../../../components/Footer";
-import UserLayout from "../../src/app/components/other/UserLayout";
-import HomePage from "../../src/app/components/home/HomePage";
-import AuthRequired from "../../src/app/components/other/AuthRequired";
-
-
+import UserLayout from '../../src/app/components/other/UserLayout';
+import HomePage from '../../src/app/components/home/HomePage';
+import AuthRequired from '../../src/app/components/other/AuthRequired';
 
 export default function Home() {
-    return (
+  return (
     <UserLayout pageTitle="Dreamjobpal | Linkedin">
-        {/* <PageHeader title="Linkedin Contacts Details" /> */}
-        {/* <AuthRequired redirectPath="/homepage" mustAdmin={false}> */}
-
-        <div className="col-md-6 p-3">
-            <HomePage/>
-        </div>
-        {/* </AuthRequired> */}
-        {/* <Footer /> */}
+      {/* <PageHeader title="Linkedin Contacts Details" /> */}
+      {/* <AuthRequired redirectPath="/homepage" mustAdmin={false}> */}
+      <div className="col-md-6 p-3">
+        <HomePage />
+      </div>
+      {/* </AuthRequired> */}
+      {/* <Footer /> */}
     </UserLayout>
-    );
+  );
 }
-
-
-
-
-
-
---
+++ packages/web/pages/thank-you/index.js
@@ -0,0 +1,14 @@
+import UserLayout from '../../src/app/components/other/UserLayout';
+import HomePage from '../../src/app/components/home/HomePage';
+
+export default function Home() {
+  return (
+    <UserLayout pageTitle="Dreamjobpal | Thank you">
+      <div className="py-5">
+        <div>
+          <h1 className="text-center">THANK YOU</h1>
+        </div>
+      </div>
+    </UserLayout>
+  );
+}
