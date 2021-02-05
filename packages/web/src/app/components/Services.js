b2e63191f51c8bc132513f9142e996a2cedfd999
Sumithra Jonnalagadda <sumithrajonnalagadda@Bills-MBP.home>
2021-02-05 04:19:52 -0800

    services updated

--- packages/web/src/app/components/Services.js
+++ packages/web/src/app/components/Services.js
@@ -23,57 +23,6 @@ const Services = () => {
                              data-wow-delay="100ms">
                             <div className="service-one__single">
                                 <i className="zimed-icon-computer-graphic"></i>
-                                <h3>Contracts</h3>
-                            </div>
-
-                        </div>
-
-                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
-                             data-wow-delay="200ms">
-                            <div className="service-one__single">
-                                <i className="zimed-icon-development1"></i>
-                                <h3>Payments</h3>
-                            </div>
-
-                        </div>
-
-                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
-                             data-wow-delay="300ms">
-                            <div className="service-one__single">
-                                <i className="zimed-icon-development"></i>
-                                <h3>Reports</h3>
-                            </div>
-
-                        </div>
-
-                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
-                             data-wow-delay="400ms">
-                            <div className="service-one__single">
-                                <i className="zimed-icon-development"></i>
-                                <h3>Smart Assitsant </h3>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="container">
-                    
-                    <div className="row">
-                        {/* <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
-                             data-wow-delay="000ms">
-                            <div className="service-one__single">
-                                <i className="zimed-icon-responsive"></i>
-                                <h3>Free Setup</h3>
-                            </div>
-                        </div> */}
-
-                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
-                             data-wow-delay="100ms">
-                            <div className="service-one__single">
-                                <i className="zimed-icon-computer-graphic"></i>
                                 <h3>Appointments</h3>
                             </div>
 
@@ -102,6 +51,57 @@ const Services = () => {
                             <div className="service-one__single">
                                 <i className="zimed-icon-development"></i>
                                 <h3>Classes </h3>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="container">
+                    
+                    <div className="row">
+                        {/* <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
+                             data-wow-delay="000ms">
+                            <div className="service-one__single">
+                                <i className="zimed-icon-responsive"></i>
+                                <h3>Free Setup</h3>
+                            </div>
+                        </div> */}
+
+                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
+                             data-wow-delay="100ms">
+                            <div className="service-one__single">
+                                <i className="zimed-icon-computer-graphic"></i>
+                                <h3>Contracts</h3>
+                            </div>
+
+                        </div>
+
+                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
+                             data-wow-delay="200ms">
+                            <div className="service-one__single">
+                                <i className="zimed-icon-development1"></i>
+                                <h3>Payments</h3>
+                            </div>
+
+                        </div>
+
+                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
+                             data-wow-delay="300ms">
+                            <div className="service-one__single">
+                                <i className="zimed-icon-development"></i>
+                                <h3>Reports</h3>
+                            </div>
+
+                        </div>
+
+                        <div className="service-one__col wow fadeInUp" data-wow-duration="1500ms"
+                             data-wow-delay="400ms">
+                            <div className="service-one__single">
+                                <i className="zimed-icon-development"></i>
+                                <h3>Smart Assitsant </h3>
                             </div>
 
                         </div>
